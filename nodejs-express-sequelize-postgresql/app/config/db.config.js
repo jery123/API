c4117ff6b41161d245de8f28a38769f1018181a6
foto jery <jery.foto@jangolo.com>
2021-07-01 17:56:49 +0100

    4e

---
+++ nodejs-express-sequelize-postgresql/app/config/db.config.js
@@ -0,0 +1,27 @@
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "",
+//     DB: "testdb",
+//    // dialect: "postgres",
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
+//     }
+//   };
+
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "",
+    DB: "testdb",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
